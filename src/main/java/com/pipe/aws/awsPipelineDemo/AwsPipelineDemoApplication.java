cf70a9104dad9277c8fc5b19a7f87ed3f3589fa8
liang <xiaoleliang045@gmail.com>
2021-10-26 17:46:08 +0900

    first commit

---
+++ src/main/java/com/pipe/aws/awsPipelineDemo/AwsPipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pipe.aws.awsPipelineDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPipelineDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPipelineDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipe/aws/awsPipelineDemo/AwsPipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipe.aws.awsPipelineDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPipelineDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
